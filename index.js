e39a8992cb15fb6adb3f2b7e53d067d82546b558
Mohith Shrivastava <mohith.shrivastava@salesforce.com>
2021-03-24 23:19:48 -0400

    temp test

--- index.js
+++ index.js
@@ -28,24 +28,25 @@ app.get("/", async (req, res) => {
     });
 
     const lead = { LastName: "Mohith", Company: "Salesforce" };
-    const campaign = { Name: "Test Campaign" };
-    const campaignMember = {
-      campaignId: "@{Campaign1.Id}",
-      leadId: "@{lead1.Id}",
-    };
-
     const leadInput = new Request.CompositeSubRequest(
       "POST",
       "/services/data/v51.0/sobjects/Lead/",
       "Lead1",
       lead
     );
+
+    const campaign = { Name: "Test Campaign" };
     const campaignInput = new Request.CompositeSubRequest(
       "POST",
       "/services/data/v51.0/sobjects/Campaign/",
       "Campaign1",
       campaign
     );
+
+    const campaignMember = {
+      campaignId: "@{Campaign1.Id}",
+      leadId: "@{Lead1.Id}",
+    };
     const campiagnMemberInput = new Request.CompositeSubRequest(
       "POST",
       "/services/data/v51.0/sobjects/CampaignMember/",
